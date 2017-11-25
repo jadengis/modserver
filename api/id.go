74005fbcf3c406e6c5c96f2ffe4bcedc9126dc96
jadengis <jadengis@gmail.com>
2017-11-25 00:59:36 -0500

    finish customer api

--- api/id.go
+++ api/id.go
@@ -1,14 +1,31 @@
 package api
 
+import (
+	"github.com/jadengis/modserver/duid"
+)
+
+var (
+	idGenerator duid.IdGenerator
+)
+
+// SetIdGenerator sets the IdGenerator for the api package to use.
+func SetIdGenerator(generator duid.IdGenerator) {
+	idGenerator = generator
+}
+
 // Id is the base identification element to be used by all api elements.
 type Id int
 
-// IdEntity is an interface for Id holding api elements.
+// IdEntity is a type containing a unique Id.
+//
+// Id returns the id of the IdEntity.
+// SetId sets the id of the IdEntity.
 type IdEntity interface {
-	GetId() Id
+	Id() Id
+	SetId(Id)
 }
 
-// generateId creates a new unique Id to be used by an api element.
-func generateId() Id {
-
+// newId creates a new unique Id to be used by an IdEntity.
+func newId() Id {
+	return Id(idGenerator.NewId())
 }
